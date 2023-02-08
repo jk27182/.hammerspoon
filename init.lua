2d8cba12cd3fff2242780cd5e9054750e9a4ad9f
Janik Königshofer <janik@koenigshofer-hh.de>
2023-02-08 11:39:30 +0100

    Add shortcut for kitty

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
+-- Shell ausführungen
+function shell_cmd(name, bool)
+	return function()
+		hs.execute(name, bool)
+	end
+end
+hs.hotkey.bind({"shift", "alt"}, "N", shell_cmd("open -a iTerm ~/scripts/make_note.sh", true))
+hs.hotkey.bind({"shift", "alt"}, "V", shell_cmd("open -a iTerm ~/scripts/neovim_browser.sh", true))
+hs.hotkey.bind({"shift", "alt"}, "T", shell_cmd("open -a iTerm ~/scripts/todos_in_notes.sh", true))
+
 --- start quick open applications 
 function open_app(name)
     return function()
@@ -7,18 +17,42 @@ function open_app(name)
         end
     end
 end
+-- Screenshot to Clipboard
+--hs.hotkey.bind({"cmd", "shift"}, "4", {"cmd", "shift", "ctrl", "4"})
+hs.hotkey.bind({"cmd"}, "G", function() hs.eventtap.keyStroke({"cmd", "shift", "ctrl"}, "4") end)
+
+--
 --- quick open applications
 hs.hotkey.bind({"shift", "alt"}, "S", open_app("Safari"))
 hs.hotkey.bind({"shift", "alt"}, "M", open_app("Mail"))
 hs.hotkey.bind({"shift", "alt"}, "E", open_app("Emacs"))
-hs.hotkey.bind({"shift", "alt"}, "N", open_app("Reminders"))
+-- hs.hotkey.bind({"shift", "alt"}, "N", open_app("Reminders"))
 hs.hotkey.bind({"shift", "alt"}, "R", open_app("RStudio"))
 hs.hotkey.bind({"shift", "alt"}, "C", open_app("Visual Studio Code"))
 hs.hotkey.bind({"shift", "alt"}, "F", open_app("Finder"))
 hs.hotkey.bind({"shift", "alt"}, "G", open_app("Google Chrome"))
-hs.hotkey.bind({"shift", "alt"}, "V", open_app("Preview"))
+hs.hotkey.bind({"shift", "alt"}, "P", open_app("Preview"))
 --- end quick open applications
 
+hs.hotkey.bind({"shift", "alt"}, "T", function()
+  local app = hs.application.get("kitty")
+  if app then
+      if not app:mainWindow() then
+          app:selectMenuItem({"kitty", "New OS window"})
+      elseif app:isFrontmost() then
+          app:hide()
+      else
+          app:activate()
+      end
+  else
+      hs.application.launchOrFocus("kitty")
+      app = hs.application.get("kitty")
+  end
+
+  app:mainWindow():moveToUnit'[100,50,0,0]'
+  app:mainWindow().setShadows(false)
+end)
+-- paired with hide_window_decorations yes in kitty.conf it makes for a very viable alternative to iTerm
 -- hs.hotkey.bind({"alt", "shift"}, "H", function()
 --   local win = hs.window.focusedWindow()
 --   local f = win:frame()
@@ -38,7 +72,7 @@ function magnetizeWindowLeft()
 	print(app:getMenuItems())
 	app:selectMenuItem({"Window", "Move Window to Left Side of Screen"})
 end
-hs.hotkey.bind({"shift", "alt"}, 'P', magnetizeWindowLeft)
+-- hs.hotkey.bind({"shift", "alt"}, 'P', magnetizeWindowLeft)
 --
 hs.hotkey.bind({"shift", "alt"}, "H", function()
   local win = hs.window.focusedWindow()
